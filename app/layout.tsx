3c5d32969d23087289556eaacda38e4c287945d4
Gabriel Baims <gbaims@users.noreply.github.com>
2025-10-29 13:44:19 -0300

    add prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,6 @@
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
         {/* Layout UI */}
         {/* Place children where you want to render a page or nested layout */}
